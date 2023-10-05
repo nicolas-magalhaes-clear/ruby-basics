7ec1090251e1a3e5378f2370bcbf079337982bff
Nicolas Magalhaes <nicolas.magalhaes.clear@gmail.com>
2023-10-05 12:04:19 -0300

    added: Synbols explanations and examples

---
+++ 9 - Symbols.rb
@@ -0,0 +1,18 @@
+#Simbols are indentificators
+#Most used when its needed guarantes his use will not inflict
+#On creation of new objects
+#Mostly used in hases
+
+
+userData = {:name => "Nicolas", :password => "Senha123"}
+
+
+print("Name:", userData[:name], "\n")
+print("Password:", userData[:password], "\n")
+
+
+newUserData = { name: 'Nicolas', surname: 'Bispo'} 
+
+
+print('New user data', newUserData, "\n")
+print('User new data name:', newUserData[:name])
