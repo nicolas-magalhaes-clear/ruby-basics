1ba8abd4cddeba1284aa14567fd0c1005cd69dff
Nicolas Magalhaes <nicolas.magalhaes.clear@gmail.com>
2023-10-05 12:03:21 -0300

    added: stdout  explanations and examples

---
+++ 2 - stdout.rb
@@ -0,0 +1,13 @@
+#To print data um console whe use the PUTS command
+
+
+
+puts "Hello world!"
+
+
+puts "Hello world" + " Concatened element"
+
+
+#When using semicolon it makes jump to line bellow
+puts "Hello world plus " , 10
+
